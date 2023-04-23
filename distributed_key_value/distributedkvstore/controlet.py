ad861c1f787c936c798cad689dfbf1ba9f2edb92
Aditya Arun Rudrawar <arudrawa@iu.edu>
2023-04-23 18:27:21 -0400

    Format python file

--- distributed_key_value/distributedkvstore/controlet.py
+++ distributed_key_value/distributedkvstore/controlet.py
@@ -4,11 +4,12 @@ import socket
 import threading
 import heapq
 
+
 class Request:
     def __init__(self):
-        self.clock : float
+        self.clock: float
         self.message = ''
-        
+
 
 class Controlet(Process):
     def __init__(self, address, id, datalets):
@@ -16,28 +17,30 @@ class Controlet(Process):
 
         self.address = address
 
-        self.clock =  float("1." + str(id))
+        self.counter = 0
+
+        self.id = id
 
         self.node_datalet = datalets[id]
-        
+
         self.queue = []
         self.sorting_lambda = lambda x: (x[0], x[1])
 
         self.other_datalets = datalets[:id] + datalets[id + 1:]
-        
-        print("clock: ",self.clock)
 
- 
+        print("clock: ", self.clock)
+
+    def __getClock(self):
+        return float(str(self.counter) + "." + str(self.id))
+
     def __broadcastWrite(self, key, value):
         for i in range(self.other_datalets):
             c = Client(self.other_datalets[i])
             c.set(key, value)
             c.close()
 
-
     def __addToQueue(self, ele):
         heapq.heappush()
-
 
     def __setKey(self, conn, key, value):
         c = Client(self.node_datalet)
@@ -46,7 +49,6 @@ class Controlet(Process):
 
         self.__broadcastWrite(key, value)
         c.close()
-    
 
     def __getKey(self, conn, key):
         c = Client(self.node_datalet)
@@ -54,10 +56,9 @@ class Controlet(Process):
         conn.sendall(reponse)
         c.close()
 
-
     def run(self):
         print("Controlet process started")
-        
+
         # Start the listening process.
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -71,23 +72,25 @@ class Controlet(Process):
                 conn, addr = serverSocket.accept()
 
                 request = conn.recv(4096)
-                
+
                 request = request.decode('utf8')
 
                 if request[:3] == "set":
                     split_resp = request.split(" ")
-                    print("set key: ",split_resp[1])
-                    print("set value: ",split_resp[2].split('\r\n')[0])
+                    print("set key: ", split_resp[1])
+                    print("set value: ", split_resp[2].split('\r\n')[0])
 
-                    threading.Thread(target=self.__setKey, args=( conn, split_resp[1], split_resp[2].split('\r\n')[0],)).start()
+                    threading.Thread(target=self.__setKey, args=(
+                        conn, split_resp[1], split_resp[2].split('\r\n')[0],)).start()
                 elif request[:3] == "get":
                     split_resp = request.split(" ")
-                    print("get key: ",split_resp[1])
-                    threading.Thread(target=self.__getKey, args=(conn, split_resp[1].split('\r\n')[0],)).start()
+                    print("get key: ", split_resp[1])
+                    threading.Thread(target=self.__getKey, args=(
+                        conn, split_resp[1].split('\r\n')[0],)).start()
                 else:
                     conn.sendall(b"INVALID_COMMAND\r\n")
             except Exception as e:
                 print(f"Exception raised in: {self.clock}")
                 print(e)
-            
+
             id += 1
